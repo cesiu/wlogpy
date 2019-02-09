cbbfd02c4aa6d04b4e2632f915071269791641c7
Christopher Siu <christopher@siu.sh>
2019-02-08 20:25:09 -0800

    Fixed using the callee's globals.

--- wlog.py
+++ wlog.py
@@ -1,19 +1,26 @@
+# Brings the magic of "without loss of generality" to Python programs.
+# Christopher Siu (cesiu@calpoly.edu)
+
 import itertools
 import inspect
 import ctypes
 
 
+# Encapsulates WLOG functionality in a context manager so that it can be
+#  invoked using the existing 'with' keyword.
 class out_loss_of_generality:
     def __init__(self, predicate):
         self.predicate = predicate
         self.shadowed_locals = {}
         self.shadowed_globals = {}
 
+    # Tries to rebind identifiers in the caller's environment to satisfy the
+    #  given predicate.
     def __enter__(self):
-        # Get the parent's local and global environments.
+        # Get the caller's local and global environments.
         frame = inspect.stack()[1].frame
         local_env = frame.f_locals
-        global_env = globals()
+        global_env = frame.f_globals
         environment = global_env.copy()
         environment.update(local_env)
 
@@ -27,39 +34,35 @@ class out_loss_of_generality:
             else:
                 raise Exception("Error: Identifier '%s' unbound." % identifier)
 
-        # First, try all permutations of locals only.
-        for bindings in itertools.permutations(local_env, len(identifiers)):
+        # First, try all permutations of locals, then, of all values.
+        if not self.shadow(
+               identifiers, local_env, local_env, environment, frame) \
+           and not self.shadow(
+               identifiers, local_env, environment, environment, frame):
+            raise Exception("Error: No satisfying assignments found.")
+
+    # Tries all permutations of a given set of bindings, committing the first
+    #  that satifies the given predicate.
+    # Returns true if a satisfying assignment was found, false otherwise.
+    def shadow(self, identifiers, dest_env, src_env, environment, frame):
+        for bindings in itertools.permutations(src_env, len(identifiers)):
             try:
-                if self.predicate(*(local_env[binding]
-                                    for binding in bindings)):
+                if self.predicate(*(src_env[binding] for binding in bindings)):
                     for identifier, binding in zip(identifiers, bindings):
-                        local_env[identifier] = environment[binding]
+                        dest_env[identifier] = environment[binding]
                         ctypes.pythonapi.PyFrame_LocalsToFast(
                          ctypes.py_object(frame), ctypes.c_int(0))
-                    return
+                    return True
             except TypeError:
                 pass
+        return False
 
-        # Then, try all permutations of all values in the environment.
-        for bindings in itertools.permutations(environment, len(identifiers)):
-            try:
-                if self.predicate(*(environment[binding]
-                                    for binding in bindings)):
-                    for identifier, binding in zip(identifiers, bindings):
-                        local_env[identifier] = environment[binding]
-                        ctypes.pythonapi.PyFrame_LocalsToFast(
-                         ctypes.py_object(frame), ctypes.c_int(0))
-                    return
-            except TypeError:
-                pass
-
-        raise Exception("Error: No satisfying assignments found.")
-
+    # Restores bindings changed by the context manager.
     def __exit__(self, exc_type, exc_value, traceback):
-        # Restore the parent's local and global environments
+        # Restore the caller's local and global environments
         frame = inspect.stack()[1].frame
         local_env = frame.f_locals
-        global_env = globals()
+        global_env = frame.f_globals
 
         for identifier in self.shadowed_locals:
             local_env[identifier] = self.shadowed_locals[identifier]
@@ -68,24 +71,3 @@ class out_loss_of_generality:
 
         for identifier in self.shadowed_globals:
             global_env[identifier] = self.shadowed_globals[identifier]
-
-x = 0
-w = 4
-
-def main():
-    x = 1
-    y = 2
-    z = 3
-
-    print("Before -- x: %d y: %d z: %d w: %d" % (x, y, z, w))
-
-    with out_loss_of_generality(lambda x, y: x > y):
-        print("During -- x: %d y: %d z: %d w: %d" % (x, y, z, w))
-        z = 4
-        print("During -- x: %d y: %d z: %d w: %d" % (x, y, z, w))
-
-    print("After  -- x: %d y: %d z: %d w: %d" % (x, y, z, w))
-
-
-if __name__ == "__main__":
-    main()

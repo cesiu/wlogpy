07345cd9bee6ce9a8fbf428be0a9832f3c91a637
Christopher Siu <christopher@siu.sh>
2019-02-08 19:18:12 -0800

    Accounted for globals.

--- wlog.py
+++ wlog.py
@@ -1,63 +1,90 @@
-import copy
 import itertools
 import inspect
 import ctypes
 
 
 class out_loss_of_generality:
-    def __init__(self, predicate, *bindings):
+    def __init__(self, predicate):
         self.predicate = predicate
-        self.bindings = bindings
+        self.shadowed_locals = {}
+        self.shadowed_globals = {}
 
     def __enter__(self):
-        # Get the parent's frame and save the existing scope.
-        scope = inspect.stack()[1].frame
-        self.saved = copy.deepcopy(scope.f_locals)
-        self.shadowed = []
+        # Get the parent's local and global environments.
+        frame = inspect.stack()[1].frame
+        local_env = frame.f_locals
+        global_env = globals()
+        environment = global_env.copy()
+        environment.update(local_env)
 
-        # Figure out which locals we might want to change.
-        bindings = list(inspect.signature(self.predicate).parameters.keys())
-        for binding in bindings:
-            if binding not in scope.f_locals:
-                raise Exception("Error: '%s' unbound." % binding)
+        # Figure out which bindings we might need to change.
+        identifiers = list(inspect.signature(self.predicate).parameters.keys())
+        for identifier in identifiers:
+            if identifier in local_env:
+                self.shadowed_locals[identifier] = local_env[identifier]
+            elif identifier in global_env:
+                self.shadowed_globals[identifier] = global_env[identifier]
             else:
-                self.shadowed.append(binding)
+                raise Exception("Error: Identifier '%s' unbound." % identifier)
 
-        # Try all the permutations of locals.
-        for locals in itertools.permutations(scope.f_locals, len(bindings)):
-            if self.predicate(*(scope.f_locals[local] for local in locals)):
-                for binding, local in zip(bindings, locals):
-                    scope.f_locals[binding] = self.saved[local]
-                    ctypes.pythonapi.PyFrame_LocalsToFast(
-                     ctypes.py_object(scope), ctypes.c_int(0))
-                break
-        else:
-            raise Exception("Error: lost generality.")
+        # First, try all permutations of locals only.
+        for bindings in itertools.permutations(local_env, len(identifiers)):
+            try:
+                if self.predicate(*(local_env[binding]
+                                    for binding in bindings)):
+                    for identifier, binding in zip(identifiers, bindings):
+                        local_env[identifier] = environment[binding]
+                        ctypes.pythonapi.PyFrame_LocalsToFast(
+                         ctypes.py_object(frame), ctypes.c_int(0))
+                    return
+            except TypeError:
+                pass
 
+        # Then, try all permutations of all values in the environment.
+        for bindings in itertools.permutations(environment, len(identifiers)):
+            try:
+                if self.predicate(*(environment[binding]
+                                    for binding in bindings)):
+                    for identifier, binding in zip(identifiers, bindings):
+                        local_env[identifier] = environment[binding]
+                        ctypes.pythonapi.PyFrame_LocalsToFast(
+                         ctypes.py_object(frame), ctypes.c_int(0))
+                    return
+            except TypeError:
+                pass
+
+        raise Exception("Error: No satisfying assignments found.")
 
     def __exit__(self, exc_type, exc_value, traceback):
-        # Get the parent's frame and restore the old scope.
-        scope = inspect.stack()[1].frame
+        # Restore the parent's local and global environments
+        frame = inspect.stack()[1].frame
+        local_env = frame.f_locals
+        global_env = globals()
 
-        for binding in self.shadowed:
-            scope.f_locals[binding] = self.saved[binding]
+        for identifier in self.shadowed_locals:
+            local_env[identifier] = self.shadowed_locals[identifier]
             ctypes.pythonapi.PyFrame_LocalsToFast(
-             ctypes.py_object(scope), ctypes.c_int(0))
+             ctypes.py_object(frame), ctypes.c_int(0))
 
+        for identifier in self.shadowed_globals:
+            global_env[identifier] = self.shadowed_globals[identifier]
+
+x = 0
+w = 4
 
 def main():
     x = 1
     y = 2
     z = 3
 
-    print("Before -- x: %d y: %d z: %d" % (x, y, z))
+    print("Before -- x: %d y: %d z: %d w: %d" % (x, y, z, w))
 
     with out_loss_of_generality(lambda x, y: x > y):
-        print("During -- x: %d y: %d z: %d" % (x, y, z))
+        print("During -- x: %d y: %d z: %d w: %d" % (x, y, z, w))
         z = 4
-        print("During -- x: %d y: %d z: %d" % (x, y, z))
+        print("During -- x: %d y: %d z: %d w: %d" % (x, y, z, w))
 
-    print("After  -- x: %d y: %d z: %d" % (x, y, z))
+    print("After  -- x: %d y: %d z: %d w: %d" % (x, y, z, w))
 
 
 if __name__ == "__main__":
